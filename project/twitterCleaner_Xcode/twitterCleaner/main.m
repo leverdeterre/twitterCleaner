69f2b37705ead9e6dd1955ff55e8668b402e5c12
jerome Morissard <morissardj@gmail.com>
2014-05-01 16:29:50 +0200

    update

--- project/twitterCleaner_Xcode/twitterCleaner/main.m
+++ project/twitterCleaner_Xcode/twitterCleaner/main.m
@@ -24,12 +24,15 @@ int main(int argc, const char * argv[])
                 NSLog(@"ok block, %@",twitter);
                 
                 for (NSString *userId in ids) {
-                    [twitter postFriendshipsDestroyScreenName:nil orUserID:userId successBlock:^(NSDictionary *unfollowedUser) {
+                    /*[twitter postFriendshipsDestroyScreenName:nil orUserID:userId successBlock:^(NSDictionary *unfollowedUser) {
                         NSLog(@"unfollowed UserId %@ ",userId);
 
                     } errorBlock:^(NSError *error) {
                         NSLog(@"errorBlock %@",error);
                     }];
+                     */
+                    NSLog(@"unfollowed UserId %@ ",userId);
+
                 }
 
             } errorBlock:^(NSError *error) {
